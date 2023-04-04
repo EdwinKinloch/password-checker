4c6281c4baf125bfd12beb74d5d6a271b7ef6a06
Edwin <49170990+EdwinKinloch@users.noreply.github.com>
2023-04-04 18:27:30 -0500

    Removed print
    This function was used for troubleshooting.

--- passwordcheck.py
+++ passwordcheck.py
@@ -48,7 +48,6 @@ def pwned_api_check(password):
     sha1password = hashlib.sha1(password.encode('utf-8')).hexdigest().upper()
     first5_char, tail = sha1password[:5], sha1password[5:]
     response = request_api_data(first5_char)
-    print(response)
     return get_password_leaks_count(response, tail)
 
 def main(args):
