de8c7397ba3b0bddcdb24f2291969735273f51b2
Edwin <49170990+EdwinKinloch@users.noreply.github.com>
2023-04-08 13:35:14 -0500

    Initial upload

---
+++ app.py
@@ -0,0 +1,14 @@
+from flask import Flask, request, jsonify
+from passwordcheck import pwned_api_check
+import getpass
+
+app = Flask(__name__)
+
+@app.route('/check_password', methods=['POST'])
+def check_password():
+    password = request.form.get('password')
+    count = pwned_api_check(password)
+    return jsonify({"count": count})
+
+if __name__ == '__main__':
+    app.run(debug=True)
